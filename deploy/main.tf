99367128cb80aa6b1f4737eba0f5b0b96f09cbf2
prudhvi8009 <149175603+prudhvi8009@users.noreply.github.com>
2023-10-31 09:09:15 -0500

    project final test merged all commits to single commit

---
+++ deploy/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  backend "s3" {
+    bucket         = "c3-recipe-app-api-devops-tfstate"
+    key            = "recipe-app.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "recipe-app-api-devops-tf-state-lock"
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  version = "~> 2.54.0"
+}
+
+locals {
+  prefix = "${var.prefix}-${terraform.workspace}"
+  common_tags = {
+    Environment = terraform.workspace
+    Project     = var.project
+    Owner       = var.contact
+    ManagedBy   = "Terraform"
+  }
+}
+
+data "aws_region" "current" {}
---
+++ deploy/variables.tf
@@ -0,0 +1,52 @@
+variable "prefix" {
+  default = "raad"
+}
+
+variable "project" {
+  default = "recipe-app-api-devops"
+}
+
+variable "contact" {
+  default = "email@gmail.com"
+}
+
+variable "db_username" {
+  description = "Username for the RDS postgres instance"
+}
+
+variable "db_password" {
+  description = "Password for the RDS postgres instance"
+}
+
+variable "bastion_key_name" {
+  default = "recipe-app-api-devops-bastion"
+}
+
+variable "ecr_image_api" {
+  description = "ECR image for API"
+  default     = "875086615781.dkr.ecr.us-east-1.amazonaws.com/recipe-app-api-devops:latest"
+}
+
+variable "ecr_image_proxy" {
+  description = "ECR image for proxy"
+  default     = "875086615781.dkr.ecr.us-east-1.amazonaws.com/recipe-app-api-proxy:latest"
+}
+
+variable "django_secret_key" {
+  description = "Secret key for Django app"
+}
+
+variable "dns_zone_name" {
+  description = "Domain name"
+  default     = ""
+}
+
+variable "subdomain" {
+  description = "Subdomain per environment"
+  type        = map(string)
+  default = {
+    production = "api"
+    staging    = "api.staging"
+    dev        = "api.dev"
+  }
+}
